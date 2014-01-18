33514d4d2418784047caab8da6b02dc43effe0ff
Yan Wu <yanvv@yanvv-mbp.local>
2014-01-18 11:52:16 -0800

    update

---
+++ .oh-my-zsh/custom/my.zsh
@@ -0,0 +1,6 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+(tmux ls | grep -vq attached && tmux -2 at) || tmux -2
