db1139bb3d9ddcb0cba6350ca86f3fe139782139
teloon <telooon@gmail.com>
2015-09-07 17:46:41 -0700

    rm zsh related

--- .oh-my-zsh/custom/my.zsh
+++
@@ -1,6 +0,0 @@
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
-#(tmux ls | grep -vq attached && tmux -2 at) || tmux -2
